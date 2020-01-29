b0e01d0ad7c4a3ce96db60c3eb00272a7ba3a1e3
yeopgi <7677942@hanmail.net>
2020-01-29 09:47:55 +0900

    2 cpp file added

--- baekjoon/c++/11053.cpp
+++ baekjoon/c++/11053.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <cstdio>
 #include <algorithm>
 using namespace std;
@@ -24,6 +23,7 @@ int main(void){
                     temp = Dp[j];
             }
         }
+        
         Dp[i] = temp + 1;
     }
  
@@ -31,40 +31,3 @@ int main(void){
 
     return 0;
 }
-=======
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int d[1000 + 1];
-
-int main(void)
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int size;
-    cin >> size;
-    vector<int> v(size + 1);
-    for(int i = 1; i <= size; i++) {
-        cin >> v[i];
-    }
-
-    d[1] = 1;
-    for(int i = 2; i <= size; i++) {
-        int max = d[1], cnt = 0;
-        for (int j = 1; j <= i; j++) {
-            if (v[i] > v[j]) {
-                cnt++;
-            }
-        }
-
-        d[i] = d[i - 1];
-
-    }
-
-    cout << *max_element(d, d + size);
-    return 0;
-}
->>>>>>> b24fca794516c0210d9885c8476e3cffd4833676
