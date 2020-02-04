0ceb3fc7e6e8255125392f9fcd4fa2433ace774f
yeopgi <7677942@hanmail.net>
2020-02-04 17:15:49 +0900

    1260 cpp modified

--- baekjoon/c++/1260.cpp
+++ baekjoon/c++/1260.cpp
@@ -1,8 +1,8 @@
-#include <algorithm>
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
 void Dfs(int start);
@@ -13,47 +13,47 @@ vector<vector<int>> v;
 queue<int> q;
 int vertex, edge, startVertex;
 
-int main(void) {
+int main(void)
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
     cin >> vertex >> edge >> startVertex;
     v.resize(vertex + 1);
-    for(int i = 0; i < vertex + 1; i++) {
-        v[i].resize(vertex + 1);
-    }
-
+    
     while (edge--) {
         int from, to;
         cin >> from >> to;
-        v[from][to] = to;
-        v[to][from] = from;
+        v[from].push_back(to);
+        v[to].push_back(from);
+    }
+
+    for (int i = 0; i < vertex; i++) {
+        sort(v[i].begin(), v[i].end());
     }
 
     Dfs(startVertex);
 
     cout << '\n';
-    fill_n(visit, 10001, false);
-
+    fill_n(visit, vertex + 1, false);
+    
     Bfs(startVertex);
 
     return 0;
 }
 
-void Dfs(int start) 
-{
+void Dfs(int start)
+ {
     visit[start] = true;
     cout << start << ' ';
-    for (int i = 1; i < v[start].size(); i++) {
-        if (v[start][i] != 0) {
-            if (!visit[v[start][i]]) {
-                Dfs(v[start][i]);
-            }
+    for (int i = 0; i < v[start].size(); i++) {
+        if (!visit[v[start][i]]) {
+            Dfs(v[start][i]);
         }
     }
 }
 
-void Bfs(int start) 
+void Bfs(int start)
 {
     q.push(start);
     visit[start] = true;
@@ -61,13 +61,10 @@ void Bfs(int start)
         int x = q.front();
         q.pop();
         cout << x << ' ';
-        for (int i = 1; i < v[x].size(); i++) {
-            int temp = v[x][i];
-            if (temp != 0) {
-                if (!visit[temp]) {
-                    q.push(temp);
-                    visit[temp] = true;
-                }
+        for (int y : v[x]) {
+            if (!visit[y]) {
+                q.push(y);
+                visit[y] = true;
             }
         }
     }
