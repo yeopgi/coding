36412d40576a7d0e5b74f7515081d7718343aac2
yeopgi <7677942@hanmail.net>
2020-02-05 16:57:19 +0900

    1931 added

---
+++ baekjoon/c++/1931.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> Information;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int meeting;
+    cin >> meeting;
+    while (meeting--) {
+
+    }
+
+    return 0;
+}
