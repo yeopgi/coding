7c4ef05f36623eb97169764bcbdd2df0512c7821
yeopgi <7677942@hanmail.net>
2020-02-12 17:06:38 +0900

    1931 modified

--- baekjoon/c++/1931.cpp
+++ baekjoon/c++/1931.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<vector<int>> information;
+typedef struct information
+{
+    int start, end;
+}information;
+
+void compare(information inform1, information inform2)
+{  
+    /*
+    if (inform1.end == inform2.end) {
+        return inform1.start < inform2.start;
+    } else {
+        return inform1.end < inform2.end;
+    }*/
+}
 
 int main(void)
 {
@@ -11,13 +25,12 @@ int main(void)
 
     int meeting;
     cin >> meeting;
-    information.resize(meeting + 1);
-    for (int i = 1;i <= meeting; i++) {
-        int start, end;
-        cin >> start >> end;
-        information[i].push_back(start);
-        information[i].push_back(end);
+    vector<information> v(meeting);
+    for (int i = 1;i <= v.size(); i++) {
+        cin >> v[i].start >> v[i].end;
     }
+
+    sort(v.begin(), v.size(), compare);
 
     return 0;
 }
