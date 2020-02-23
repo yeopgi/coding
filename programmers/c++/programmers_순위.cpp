50e08ad3891b691981d4e63939af270ff4fe1a8d
yeopgi <7677942@hanmail.net>
2020-02-23 15:43:37 +0900

    순위.cpp modified

--- programmers/c++/programmers_순위.cpp
+++ programmers/c++/programmers_순위.cpp
@@ -1,16 +1,16 @@
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <algorithm>
-
-#define INFINITE 2147483647
+#define INFINITE 10000000
 using namespace std;
 
 int check[101][101];
 
-int solution(int n, vector<vector<int>> results) {
+int solution(int n, vector<vector<int>> results) 
+{
     int answer = 0;
-    fill(&check[0][0], &check[n][n + 1], 1e6);
-    for (int i = 0; i <= results.size(); i++) {
+    fill(&check[0][0], &check[n][n + 1], INFINITE);
+    for (int i = 0; i < results.size(); i++) {
         int x = results[i][0];
         int y = results[i][1];
         check[x][y] = 1;
@@ -20,20 +20,20 @@ int solution(int n, vector<vector<int>> results) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (check[i][k] + check[k][j] < check[i][j]) {
-                    check[i][j] = check[i][k] + check[k][j]; 
+                    check[i][j] = check[i][k] + check[k][j];
                 }
             }
         }
     }
 
-    for (int i = 1; i<= n; i++) {
+    for (int i = 1; i <= n; i++) {
         bool isFight = true;
         for (int j = 1; j <= n; j++) {
             if (i == j) {
                 continue;
             }
 
-            if (check[i][j] == 1e6 && check[j][i] == 1e6) {
+            if (check[i][j] == INFINITE && check[j][i] == INFINITE) {
                 isFight = false;
             }
         }
@@ -42,6 +42,6 @@ int solution(int n, vector<vector<int>> results) {
             answer++;
         }
     }
-    
+
     return answer;
 }
