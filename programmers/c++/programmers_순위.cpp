0ace2ad300cc0462431b510054d7a94fd3369bc3
yeopgi <7677942@hanmail.net>
2020-02-23 16:15:52 +0900

    순위.cpp modified

--- programmers/c++/programmers_순위.cpp
+++ programmers/c++/programmers_순위.cpp
@@ -15,10 +15,10 @@ int solution(int n, vector<vector<int>> results)
         int y = results[i][1];
         check[x][y] = 1;
     }
-
-    for (int k = 1; k <= n; k++) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+                                                                    //플로이드와샬 알고리즘에 의하면 가장 먼저 바뀌는건 도착 노드
+    for (int k = 1; k <= n; k++) {                                  // 거쳐가는 노드   
+        for (int i = 1; i <= n; i++) {                              // 출발 노드
+            for (int j = 1; j <= n; j++) {                          // 도착 노드
                 if (check[i][k] + check[k][j] < check[i][j]) {
                     check[i][j] = check[i][k] + check[k][j];
                 }
@@ -32,8 +32,8 @@ int solution(int n, vector<vector<int>> results)
             if (i == j) {
                 continue;
             }
-
-            if (check[i][j] == INFINITE && check[j][i] == INFINITE) {
+                                                                                // 마지막까지 INFITINTE 인건 INFINITE > 비교대상일때, 비교대상이 INFINITE + K 이기 때문이다. ( 1<= K <= INFINITE)
+            if (check[i][j] == INFINITE && check[j][i] == INFINITE) {           // [i][j] 와 [j][i]를 비교하는건 서로의 전적을 알아보기 위해서
                 isFight = false;
             }
         }
