fd0ce1df1a1fb1e94b0be2314ed484cb44714558
yeopgi <7677942@hanmail.net>
2020-02-28 17:10:48 +0900

    modified

--- baekjoon/c++/2163.cpp
+++ baekjoon/c++/2163.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 int col, row;
-int d[301];
 
 int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> col >> row;
-    d[0] = 1, d[1] = 2, d[2] = 3, d[3] = 4, d[4] = 5;
     cout << col * row - 1 << '\n';
-    
+
     return 0;
 }
