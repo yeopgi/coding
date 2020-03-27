24f06c9ed390f73dedf0550fbcc7735c39ae08b0
yeopgi <ehdalsdl153@naver.com>
2020-03-27 17:58:43 +0900

    modified

--- baekjoon/c++/10448.cpp
+++ baekjoon/c++/10448.cpp
@@ -13,20 +13,29 @@ int main(void)
         cin >> k;
         int init = k;
         for (int i = 1;; i++) {
-            k -= (i * (i + 1)) / 2;
-            cnt++;
-            i = 1;
-            if (k == 0) {
-                if (cnt == 3) {
-                    cout << 1 << '\n';
-                    break;
-                } else {
+            if (k <= (i * (i + 1)) / 2) {
+                if (k == (i * (i + 1)) / 2) {
+                    if (k - ((i * (i + 1)) / 2) == 0 && cnt + 1 == 3) {
+                        cout << 1 << '\n';
+                    }
+                }
+
+                int temp = i - 1;
+                k -= (temp * (temp + 1)) / 2;
+                cnt++;
+                i = 1;
+                if (k == 0) {
+                    if (cnt == 3) {
+                        cout << 1 << '\n';
+                        break;
+                    } else {
+                        cout << 0 << '\n';
+                        break;
+                    }
+                } else if (k < 0) {
                     cout << 0 << '\n';
                     break;
                 }
-            } else if (k < 0) {
-                cout << 0 << '\n';
-                break;
             }
         }
     }
