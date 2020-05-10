33ae382c740ad22e366732095d751e6680d72d98
yeopgi <ehdalsdl153@naver.com>
2020-05-10 13:30:06 +0900

    refresh

--- programmers/c++/programmers_뉴스_클러스터링.cpp
+++ programmers/c++/programmers_뉴스_클러스터링.cpp
@@ -1,8 +1,24 @@
 #include <string>
-
+#include <vector>
+#include <sstream>
 using namespace std;
+
+vector<string> vStr1, vStr2;
+
+void split(stringstream ss, string token, char delimeter, vector<string> v)
+{
+    while (getline(ss, token, delimeter)) {
+        v.push_back(token);
+    }
+}
+
 
 int solution(string str1, string str2) {
     int answer = 0;
+    stringstream ss(str1);
+    string token;
+    split(ss, token, )
+
+
     return answer;
 }
