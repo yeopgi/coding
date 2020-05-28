1f206c263d846f70104c3eab0eaba8426cf5d2dc
yeopgi <ehdalsdl153@naver.com>
2020-05-28 19:05:58 +0900

    modify

--- baekjoon/c++/test.cpp
+++ baekjoon/c++/test.cpp
@@ -1,46 +1,65 @@
-#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <cmath>
+#include <stack>
 using namespace std;
 
-class test
-{
-protected:
-    int tmp;
-public:
-    test(){
-        tmp = 5;
-    }
-    test(int temp) : tmp(temp) {}
-    void print()
-    {
-        cout << "hello" << endl;
-    }
-    virtual ~test(){}
-};
+vector<int> option(3,0);
+vector<int> sqrtList(3, 0);
+vector<int> scoreList(3, 0);
 
-
-class test2 : public test
-{
-private:
-    int b;
-public:
-    test2(){
-        
+int Calculation(char sqrtSrc, int number) {
+    int totalScore = 0;
+    if (sqrtSrc == 'S') {
+        totalScore = pow(number, 1);
+    } else if (sqrtSrc == 'D') {
+        totalScore = pow(number, 2);
+    } else {
+        totalScore = pow(number, 3);
     }
 
-    void print()
-    {
-        cout << "hello2" << endl;
+    return totalScore;
+}
+
+int solution(string dartResult) {
+    int answer = 0, idx = option.size(), digitIdx = 2;
+    stack<int> st;
+    for (int i = 0; i < dartResult.size(); i++) {
+        st.push(dartResult[i]);
     }
 
-    test2(int temp) : b(temp) {
-        test();
-    }
-};
+    int total = 0, temp = 0;
+    int cnt = 0;
+    while (!st.empty()) {
+        char c = st.top();
+        st.pop();
 
-int main(void) 
-{
-    int temp1 = 5;
-    test2 b;
-    b.print();
-    return 0;
+        if (isdigit(c)) {
+            scoreList.push_back(Calculation(sqrtList[cnt], c - '0'));
+            if (option[cnt] == '*') {
+                scoreList[cnt] *= 2;
+            } else if (option[cnt] == '#'){
+                if (option[cnt + 1] == '*') {
+                    scoreList[cnt] *= -2;
+                } else {
+                    scoreList[cnt] *= -1;
+                }
+            }
+
+            cnt++;
+        } else {
+            if (isalpha(c)) {
+                sqrtList.push_back(c);
+            } else {
+                option[cnt] = c;
+            }
+        }
+    }
+
+    for (int i = 0; i < scoreList.size(); i++) {
+        answer += scoreList[i];
+    }
+
+    return answer;
 }
--- programmers/c++/programmers_다트_게임.cpp
+++ programmers/c++/programmers_다트_게임.cpp
@@ -1,31 +1,75 @@
 #include <string>
 #include <cctype>
 #include <vector>
-#include <cstring>
+#include <cmath>
+#include <stack>
+#include <iostream>
+
+#define INIT_VALUE 987654321
 using namespace std;
 
-vector<int> option(3, 0);
+vector<int> option(4,INIT_VALUE);
+vector<int> alphabet(4, 0);
+vector<int> score(4, 0);
+vector<int> digit(4, 0);
+
+int Calculation(char sqrtSrc, int number) {
+    int totalScore = 0;
+    if (sqrtSrc == 'S') {
+        totalScore = pow(number, 1);
+    } else if (sqrtSrc == 'D') {
+        totalScore = pow(number, 2);
+    } else {
+        totalScore = pow(number, 3);
+    }
+
+    return totalScore;
+}
+
 int solution(string dartResult) {
     int answer = 0;
-    int maxValidLength = 3, startIdx = 0;
-    for (int i = 0; i < 3; i++) {
-        int offset = 0;
-            string str = dartResult.substr(startIdx, maxValidLength - 1);
-            if (isdigit(str[2])) {
-                offset += 2;
-                if (str[1] == 'S') {
-
-                } else if (str[1] == 'D') {
-
-                } else {
-
-                }
-
-            } else {
-                offset += 3;
-                option[i] = str[2] - '0';
+    int cnt = 0;
+    for (int i = 0; i < dartResult.size(); i++) {
+        if (isdigit(dartResult[i])) {
+            cnt++;
+            if (isdigit(dartResult[i + 1])) {
+                string str = "";
+                str = dartResult.substr(i, 2);
+                digit[cnt] = stoi(str);
+                i++;
+                continue;
             }
+            digit[cnt] = dartResult[i] -'0';
+        } else {
+            if (isalpha(dartResult[i])) {
+                alphabet[cnt] = dartResult[i];
+            } else {
+                option[cnt] = dartResult[i];
+            }
+        }
+    }
 
+    for (int i = 1; i <= 3; i++) {
+        score[i] = Calculation(alphabet[i],digit[i]);
+        if (option[i] != INIT_VALUE) {
+            if (option[i] == '#') {
+                score[i] *= -1;/*
+                if (i - 1 >= 1) {
+                    if (option[i - 1] == '*') {
+                        score[i] *= 2;
+                    }
+                }*/
+            } else {
+                score[i] *= 2;
+                if (i - 1 >= 1) {
+                    score[i - 1] *= 2;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i <= score.size(); i++) {
+        answer += score[i];
     }
 
     return answer;
