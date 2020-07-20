cc8bcd99c0d6f2cef1d31dcab113804041b456cf
yeopgi <7677942@hanmail.net>
2020-07-20 23:34:18 +0900

    modify

--- baekjoon/c++/11403.cpp
+++ baekjoon/c++/11403.cpp
@@ -1,52 +1,23 @@
 #include <iostream>
-#include <queue>
+#include <cstring>
 using namespace std;
 
 int map[101][101];
-bool visit[101][101];
+bool visit[101];
 int N;
-int dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, 1, -1};
-queue<pair<int, int>> q;
 
-void floydWashall() {
-    for (int k = 1; k <= N; ++k) {
-        for (int i = 1; i <= N; ++i) {
-            for (int j = 1; j <= N; ++j) {
-                if (map[i][j] > map[i][k] + map[k][j]) {
-                    map[i][j] = map[i][k] + map[k][j];
-                }
+void Dfs(int from) {
+    for (int i = 1; i <= N; i++) {
+        if (map[from][i] == 1) {
+            if (!visit[i]) {
+                visit[i] = true;
+                Dfs(i);
             }
         }
     }
 }
 
-void Bfs(pair<int, int> src) 
-{
-    int x = src.first, y = src.second;
-    if (visit[x][y]) {
-        return;
-    }
-
-    visit[x][y] = true;
-    q.push({x, y});
-
-    while (!q.empty()) {
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i], ny = y + dy[i];
-            if (!visit[nx][ny] && nx >= 1 && nx <= N && ny >= 1 && ny <= N) {
-                if (map[nx][ny] == 1) {
-                    map[ny][nx] = 1;
-                    q.push({nx, ny});
-                }
-            }
-        }
-
-        q.pop();
-    }
-}
-
-int main(void)
-{
+int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -57,7 +28,17 @@ int main(void)
         }
     }
 
-    floydWashall();
+    for (int i = 1; i <= N; i++) {
+        memset(visit, false, sizeof(visit));
+
+        Dfs(i);
+
+        for (int j = 1; j <= N; j++) {
+            if (visit[j]) {
+                map[i][j] = 1;
+            }
+        }
+    }
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
