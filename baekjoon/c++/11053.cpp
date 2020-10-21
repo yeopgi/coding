cffb4c41da8453fe192010bf31fd7449c02b83c7
yeopgi <ehdalsdl153@naver.com>
2020-10-21 20:26:59 +0900

    add

--- baekjoon/c++/11053.cpp
+++ baekjoon/c++/11053.cpp
@@ -1,3 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int d[1001];
+int a[1001];
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> a[i];
+        d[i] = 1;
+    }
+
+    for (int i = 2; i <= N; i++) {
+        for (int j = 1; j < i; j++) {
+            if (a[i] > a[j]) {
+                if (d[i] < d[j] + 1) {
+                    d[i] = d[j] + 1;
+                }
+            }
+        }
+    }
+
+    cout << *max_element(&d[1], &d[N + 1]) << '\n';
+    return 0;
+}
+
+/*
 #include <cstdio>
 #include <algorithm>
 using namespace std;
@@ -8,7 +42,7 @@ int main(void){
     int A[1010] = {};
     int max = 0;
     scanf("%d", &N);
- 
+
     for (int i = 1; i <= N; i++) {
         scanf("%d", &A[i]);
         Dp[i] = 1;
@@ -23,11 +57,12 @@ int main(void){
                     temp = Dp[j];
             }
         }
-        
         Dp[i] = temp + 1;
     }
- 
+
     printf("%d\n", *max_element(Dp, Dp + (N + 1)));
 
     return 0;
 }
+
+*/
--- baekjoon/c++/2225.cpp
+++ baekjoon/c++/2225.cpp
@@ -1,14 +1,28 @@
+#include <cstring>
 #include <iostream>
 using namespace std;
 
-int main(void)
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+const int MAX = 200;
+const int MOD = 1000000000;
 
-    int N, K;
+int N, K;
+int cache[MAX + 1][MAX + 1]; //K, N
+
+int numOfSum(void) {
+    for (int i = 0; i <= N; i++) {
+        cache[1][i] = 1; //K=1인 경우 1가지
+    }
+
+    for (int i = 1; i <= K; i++)
+        for (int j = 0; j <= N; j++)
+            for (int l = 0; l <= j; l++)
+                cache[i][j] = (cache[i][j] + cache[i - 1][j - l]) % MOD;
+
+    return cache[K][N] % MOD;
+}
+
+int main(void) {
     cin >> N >> K;
-    
-
+    cout << numOfSum() << endl;
     return 0;
 }
