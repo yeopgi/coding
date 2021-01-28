ad53a85adf73b20cfed4dfe56c99a8f4c8d0e8a9
yeopgi <ehdalsdl153@naver.com>
2021-01-28 18:57:01 +0900

    update

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -17,14 +17,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<int> ret(2, 0);
-        map<int, int> myMap;
+        unordered_map<int, int> myMap;
         for (int i = 0; i < n; i++) {
             myMap.insert(make_pair(nums[i], i));
         }
 
         for (int i = 0; i < n; i++) {
             int temp = target - nums[i];
-            map<int, int>::iterator iter = myMap.find(temp);
+            unordered_map<int, int>::iterator iter = myMap.find(temp);
             if (iter != myMap.end() && iter->second != i) {
                 ret[0] = i;
                 ret[1] = iter->second;
