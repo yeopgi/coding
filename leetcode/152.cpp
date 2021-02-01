7baf294ed27dbbe90f9385d96d5a95c7cd9fcb3f
yeopgi <7677942@hanmail.net>
2021-02-01 17:29:19 +0900

    update

--- leetcode/152.cpp
+++ leetcode/152.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        int res = INT_MIN;
+        int dMax = 1, dMin = 1;
+        for (int i : nums) {
+            int a = i * dMax, b = i * dMin;
+            dMax = max({b, a, i});
+            dMin = min({a, b, i});
+            res = max(res, dMax);
+        }
 
+        return res;
     }
 };
