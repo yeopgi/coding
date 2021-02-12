95e4b866ec186b57997583d70beb1bfb8e48b57a
yeopgi <7677942@hanmail.net>
2021-02-12 13:29:08 +0900

    update

--- programmers/c++/programmers_주식가격.cpp
+++ programmers/c++/programmers_주식가격.cpp
@@ -1,6 +1,6 @@
+#include <stack>
 #include <string>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
@@ -8,21 +8,20 @@ vector<int> solution(vector<int> prices) {
     const int n = prices.size();
     vector<int> answer(n);
     stack<int> st;
-
-    for (int i=0; i<n; i++) {
+    for (int i = 0; i < n; i++) {
         while (!st.empty() && prices[st.top()] > prices[i]) {
             auto top = st.top();
-            st.pop();
             answer[top] = i - top;
+            st.pop();
         }
 
         st.push(i);
     }
 
     while (!st.empty()) {
-        auto top = st.top();
-        st.pop();
+        int top = st.top();
         answer[top] = n - 1 - top;
+        st.pop();
     }
 
     return answer;
