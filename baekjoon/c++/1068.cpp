e0265389030486e035dfa423852ea5c45e71effb
yeopgi <7677942@hanmail.net>
2021-03-01 14:52:22 +0900

    update

--- baekjoon/c++/1068.cpp
+++ baekjoon/c++/1068.cpp
@@ -1,39 +1,37 @@
 #include <iostream>
-#include <unordered_map>
+#include <vector>
 using namespace std;
 
 const int MAX = 50;
 int N;
-unordered_multimap<int, int> parentChild;
-unordered_map<int, int> childParent;
-int cnt = 0;
-int emptyCnt = 0;
+int delStartNode;
+pair<int, bool> parentAndisLeaf[MAX];
+vector<int> ancestor[MAX];
 
-int Run(int srcDelNode)
+int Run()
 {
-    if (!parentChild.count(srcDelNode))
-    {
-        int parent = childParent.find(srcDelNode)->second;
-        if (parentChild.count(parent) - 1 > 0) // 부모가 단말노드가 아니다.
-        {
-            return 1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < ancestor[i].size(); j++) {
+            if (ancestor[i][j] == delStartNode) {
+                parentAndisLeaf[i].second = true;
+            }
         }
-
-        return 0;
     }
 
-    int delNode = srcDelNode;
-    auto iter = parentChild.equal_range(delNode);
-    int temp = 0;
-    for (auto iter2 = iter.first; iter2 != iter.second; iter2++) {
-        temp += Run(iter2->second);
+    for (int i = 0; i < N; i++) {
+        if (i != delStartNode) {
+            parentAndisLeaf[parentAndisLeaf[i].first].second = true;            
+        }
     }
 
-    if (parentChild.count(srcDelNode) - 1 > 0) {
-        return emptyCnt - temp + 1;
-    } else {
-        return emptyCnt - temp;
+    int cnt = 0;
+    for (int i = 0; i < N; i++) {
+        if (!parentAndisLeaf[i].second) {
+            cnt++;
+        }
     }
+
+    return cnt;
 }
 
 int main(void)
@@ -42,31 +40,23 @@ int main(void)
     cin.tie(nullptr);
 
     cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        int temp;
-        cin >> temp;
-        if (i != 0) {
-            parentChild.insert({temp, i}); // (parent, child)
-            childParent.insert({i, temp}); // (parent, child)
-        }
+    for (int i = 0; i < N; i++) {
+        cin >> parentAndisLeaf[i].first;
+        parentAndisLeaf[parentAndisLeaf[i].first].second = true;
     }
 
     for (int i = 0; i < N; i++) {
-        if (!parentChild.count(i)) {
-            emptyCnt++;
+        int temp = parentAndisLeaf[i].first;
+        while (temp != -1) {
+            ancestor[i].emplace_back(temp);
+            temp = parentAndisLeaf[temp].first;
         }
     }
 
-    int delStartNode;
     cin >> delStartNode;
-    if (delStartNode == 0) {
-        cout << 0 << '\n';
-        return 0;
-    }
-    
-    int ans = Run(delStartNode);
+    parentAndisLeaf[delStartNode].second = true;
+    parentAndisLeaf[parentAndisLeaf[delStartNode].first].second = false;
 
-    cout << ans << '\n';
+    cout << Run() << '\n';
     return 0;
 }
