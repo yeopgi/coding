4dfbb5ea8b62e556a045d693d6c4ecab6fb323de
yeopgi <7677942@hanmail.net>
2021-03-28 16:16:16 +0900

    update

--- baekjoon/c++/15652.cpp
+++ baekjoon/c++/15652.cpp
@@ -5,23 +5,25 @@ const int MAX = 8 + 1;
 int N, M;
 int a[MAX];
 
-void Run(int cnt) {
-        if (cnt == M) {
-            for (int i = 0; i < M; i++) {
-                cout << a[i] << ' ';
-            }
-
-            cout << '\n';
-            return;
+void Run(int cnt)
+{
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << a[i] << ' ';
         }
 
-        for (int i = (cnt == 0) ? 1 : a[cnt - 1]; i <= N; i++) {
-            a[cnt] = i;
-            Run(cnt + 1);
-        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = (cnt == 0) ? 1 : a[cnt - 1]; i <= N; i++) {
+        a[cnt] = i;
+        Run(cnt + 1);
+    }
 }
 
-int main(void) {
+int main(void)
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
--- baekjoon/c++/15684.cpp
+++ baekjoon/c++/15684.cpp
@@ -38,12 +38,12 @@ void Run(int startRow, int cnt)
 
         if (isPossible) {
             flag = true;
-        }
 
+        }
         return;
     }
 
-    for (int i = startRow; i < N; i++) {
+    for (int i = startRow; i < H; i++) {
         for (int j = 1; j < N; j++) {
             if (!a[i][j - 1] && !a[i][j] && !a[i][j + 1]) {
                 a[i][j] = true;
