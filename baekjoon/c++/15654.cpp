84e03996d5a06b31e381cd4e7019b2398bfae26b
yeopgi <7677942@hanmail.net>
2023-01-23 17:29:10 +0900

    update

---
+++ baekjoon/c++/15654.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int         N, M;
+vector<int> my_vector;
+bool        visited[10000 + 1];
+int         ans[10000 + 1];
+
+void Run(const int start_idx, const int depth) {
+  if (visited[start_idx]) {
+    return;
+  }
+
+  visited[start_idx] = true;
+  ans[depth]         = my_vector[start_idx];
+  if (depth == M - 1) {
+    for (int i = 0; i < M; i++) {
+      cout << ans[i] << ' ';
+    }
+
+    cout << '\n';
+    visited[start_idx] = false;
+    return;
+  }
+
+  for (int i = 1; i < my_vector.size(); i++) {
+    Run(i, depth + 1);
+  }
+  visited[start_idx] = false;
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N >> M;
+  my_vector.resize(N + 1);
+  for (int i = 1; i <= N; i++) {
+    cin >> my_vector[i];
+  }
+
+  sort(my_vector.begin(), my_vector.end());
+
+  if (M == 1) {
+    for (int i = 1; i <= N; i++) {
+      cout << my_vector[i] << '\n';
+    }
+
+    return 0;
+  }
+
+  for (int i = 1; i <= N; i++) {
+    Run(i, 0);
+  }
+}
