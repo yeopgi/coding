bfc6379b924051dd2c50e37bebfcd1016f2798cd
yeopgi <7677942@hanmail.net>
2023-01-27 12:08:20 +0900

    update

--- baekjoon/python/15657.py
+++ baekjoon/python/15657.py
@@ -2,24 +2,13 @@ N, M = map(int, input().split())
 my_vector = list(map(int, input().split()))
 ans = [0 for i in range(0, 10000 + 1)]
 
-
-def is_right():
-    for i in range(0, M - 1):
-        if (ans[i] <= ans[i + 1]):
-            return True
-
-    return False
-
-
 def Run(start_idx, depth):
     ans[depth] = my_vector[start_idx]
     if (depth == M - 1):
-        if is_right() == True:
-            for i in range(0, M):
-                print(ans[i], end=' ')
+        for i in range(0, M):
+            print(ans[i], end=' ')
 
-            print('')
-
+        print('')
         return
 
     for i in range(0, len(my_vector)):
@@ -33,3 +22,4 @@ if (M == 1):
 else:
     for i in range(0, N):
         Run(i, 0)
+        my_vector.pop()
