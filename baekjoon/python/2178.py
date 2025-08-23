53640ed0812723c506c5aae4d7b61aaa038cb8c1
yeopgi <7677942@hanmail.net>
2025-08-23 15:52:22 +0900

    update

--- baekjoon/python/2178.py
+++ baekjoon/python/2178.py
@@ -6,44 +6,48 @@ dy = [0, 0, -1, 1]
 
 def Run():
     # map_str = sys.stdin.readline()
-    # maps = [sys.stdin.readline() for _ in range(n)]
-    for _ in range(n):
+    maps = [[0] * m for _ in range(n)]
+    for i in range(n):
         map_str = sys.stdin.readline()
-        print()
-        for i in range(m):
-            print(map_str[i], end ='')
+        # print()
+        for j in range(m):
+            maps[i][j] = int(map_str[j])
+            # print(maps[i][j], end ='')
 
-        print()
+        # print()
         
-    # maps = [list(map(int, input().split())) for _ in range(n)]
-    # visited = [[0] * m for _ in range(n)]
-    # my_q = deque()
-    # # print(*maps)
+    visited = [[0] * m for _ in range(n)]
+    # print(*visited)
+    my_q = deque()
+    my_q.append((0, 0, 0))
+    cnt = 0
+    while (my_q):
+        # print(1)
+        curr_coord = my_q.popleft()
+        cx = curr_coord[0]
+        cy = curr_coord[1]
+        visited[cx][cy] =1 
+        cnt = curr_coord[2] + 1
+        if (cx == n - 1 and cy == m - 1):
+            # print('abc')
+            return cnt
+        
+        # print(*maps)
+        for k in range(0, 4):
+
+            nx = cx + dx[k]
+            ny = cy + dy[k]
+            if (nx >= 0 and nx < n and ny >= 0 and ny < m):
+                if visited[nx][ny] == 0 and maps[nx][ny] == 1:
+                    my_q.append((nx, ny, cnt))
+                    visited[nx][ny] =1 
+
+    # print(cnt)
     # for i in range(0, n):
     #     for j in range(0, m):
     #         if (visited[i][j]):
     #             continue
 
     #         if (maps[i][j] == 1):
-    #             my_q.append((i, j))
-    #             cnt = 0
-    #             while (my_q):
-    #                 # print(1)
-    #                 curr_coord = my_q.popleft()
-    #                 cx = curr_coord[0]
-    #                 cy = curr_coord[1]
-    #                 visited[cx][cy] =1 
-    #                 cnt = cnt + 1
-    #                 if (cx == n and cy ==m):
-    #                     return cnt
 
-    #                 for k in range(0, 4):
-    #                     nextX = cx + dx[k]
-    #                     nextY = cy + dy[k]
-    #                     if (nextX >= 0 and nextX < n and nextY >= 0 and nextY < m):
-    #                         if visited[nextX][nextY] == 0 and maps[nextX][nextY] == 1:
-    #                             my_q.append((nextX, nextY))
-    #                             visited[nextX][nextY] =1 
-
-Run()
-# print(Run())        
+print(Run())        
--- baekjoon/python/7511.py
+++ baekjoon/python/7511.py
@@ -1,32 +1,52 @@
 import sys
 t_num = int(input())
-u_num = int(input())
 
-f_num = int(input())
 
-f_group = [-1] * (f_num )
-print(*f_group)
+f_group = [-1] * (1000001)
+# print(*f_group)
 
 def find_root(curr_num):
-    if f_group[curr_num] == -1:
+    if f_group[curr_num] < 0:
         return curr_num
     
-    root_num = find_root(curr_num)
-    return root_num
+    root_num = find_root(f_group[curr_num])
+    f_group[curr_num] = root_num
+    return f_group[curr_num]
+
 def union(num1, num2):
-    root1 = find_root(num1)
-    root2 = find_root(num2)
+    num1 = find_root(num1)
+    num2 = find_root(num2)
 
-    if root1 == root2:
-        return False
+    if num1 == num2:
+        return 0
 
-    f_group[num2] = root1
-    return True
+    if f_group[num2] < f_group[num1]:
+        temp = num1
+        num1 = num2
+        num2 = temp
 
-for _ in range(t_num):
+    if f_group[num2] == f_group[num1]:
+        f_group[num1] -= 1
+
+    f_group[num2] = num1
+    return 1
+
+for t in range(t_num):
+    for i in range(1000001):
+        f_group[i] = -1
+    u_num = int(input())
+    f_num = int(input())
     for _ in range(f_num):
         a,b= map(int ,sys.stdin.readline().split())
-        if union(a,b):
+        union(a,b)
+
+    m = int(input())
+    print(f"Scenario {t+1}:")
+    for _ in range(m):
+        u, v = map(int ,sys.stdin.readline().split())
+        if find_root(u) == find_root(v):
             print(1)
         else:
             print(0)
+        # print(find_root(u) == find_root(v))
+    print()
